2b361e8347781ad3becda61d314482866a2e9251
marco <marco@MSI>
2023-02-16 15:20:18 -0300

    Add project files.

---
+++ UserControl1.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace tarefa2
+{
+    public partial class UserControl1 : Window
+    {
+        public UIDocument uidoc { get; }
+        public Document doc { get; }
+
+        double altura = 0;
+
+        public UserControl1(UIDocument Uidoc)
+        {
+            uidoc = Uidoc;
+            doc = uidoc.Document;
+            InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IList<Element> WallTypeList = new FilteredElementCollector(doc)
+            .OfCategory(BuiltInCategory.OST_Walls).WhereElementIsElementType().ToElements();
+
+            foreach (Element wall in WallTypeList)
+            {
+                SelectingWallType.Items.Add(wall.Name);
+            }
+        }
+        public double SetNewHeigh(string text)
+        {
+            altura = Convert.ToDouble(texthigh.Text) * 3.280839895013123;
+            return altura;
+        }
+
+        public Element SelectingWallType_DataContextChanged(Document doc)
+        {
+            IList<Element> WallTypeList = new FilteredElementCollector(doc)
+            .OfCategory(BuiltInCategory.OST_Walls).WhereElementIsElementType().ToElements();
+
+            Element wallTypeSelected = null;
+            string wallTypeName = SelectingWallType.Text.ToString();
+            foreach (Element wall in WallTypeList)
+            {
+                if (wall.Name == wallTypeName)
+                {
+                    wallTypeSelected = wall;
+                    break;
+                }
+                Close();
+            }
+            return wallTypeSelected;
+        }
+        private void SetNewHeigh(object sender, RoutedEventArgs e)
+        {
+        }
+        private void SelectingWallType_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+    }
+}
---
+++ WallCreator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace tarefa2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class WallCreator : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Crianção das variáveis necessárias
+            var uiapp = commandData.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var doc = uiapp.ActiveUIDocument.Document;
+            var levelId = doc.ActiveView.GenLevel.Id;
+
+
+            //Criando um filtro para pegar todas as instâncias de Model Line, retirando os tipos de linha
+            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Lines);
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            IList<Element> lines = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+
+
+            //Inicialização da janela para o input de uma nova altura
+            UserControl1 newHigh = new UserControl1(uidoc);
+            newHigh.ShowDialog();
+
+
+            //Indicando o offset das paredes como 0 para que não saiam do local
+            double offsetWall = 0;
+
+
+            //Buscando o Id do elemento selecionado pelo usuário no ComboBox do WPF
+            ElementId wallSelectedId = newHigh.SelectingWallType_DataContextChanged(doc).Id;
+
+
+            //Iniciando a transação para criação das paredes
+            Transaction t = new Transaction(doc,"creating new walls");
+            t.Start();
+            var alturaWalls = newHigh.SetNewHeigh(newHigh.texthigh.Text);
+            foreach (Element line in lines)
+            {
+                CurveElement curve = line as CurveElement;
+                XYZ StartPoint = curve.GeometryCurve.GetEndPoint(0);
+                XYZ endPoint = curve.GeometryCurve.GetEndPoint(1);
+                Curve lineBound = Line.CreateBound(StartPoint, endPoint) as Curve;
+                Wall.Create(doc, lineBound, wallSelectedId, levelId, alturaWalls, offsetWall, false, false);
+            }
+            t.Commit();
+            return Result.Succeeded;
+        }
+
+    }
+
+}
